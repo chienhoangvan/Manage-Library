57f685b5c48e09e43d5d6a9f0e992ce2f093ae4a
chienhoangvan <chienhoangvanp@gmail.com>
2022-05-27 12:32:53 +0700

    add more book information
    add language,description, NXB, amount

--- src/main/java/com/psl/gems/model/Book.java
+++ src/main/java/com/psl/gems/model/Book.java
@@ -13,55 +13,93 @@ import javax.persistence.OneToMany;
 @Data
 @Entity
 public class Book {
-	
-	@Id
-	private long ISBN;
-	private String title;
-	private String author;
-	private String language;
 
-	private String description;
+    @Id
+    private long ISBN;
+    private String title;
+    private String author;
+    private String language;
 
-	private String NXB;
+    private String description;
 
-//	private boolean available;
+    private String NXB;
 
-	private Long amount;
-	
-	@OneToMany(mappedBy = "book", cascade = {CascadeType.REMOVE}, fetch = FetchType.LAZY, orphanRemoval = true)
-	private Set<BookObj> bookObjs;
-	
-	
-	public long getISBN() {
-		return ISBN;
-	}
-	public void setISBN(long iSBN) {
-		ISBN = iSBN;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	
-	public Set<BookObj> getBookObjs() {
-		return bookObjs;
-	}
-	public void setBookObjs(Set<BookObj> bookObjs) {
-		this.bookObjs = bookObjs;
-	}
-	@Override
-	public String toString() {
-		return "Book [ISBN=" + ISBN + ", title=" + title + ", author=" + author + "]";
-	}
-	
-	
-	
+    private Long amount;
+
+    //	private boolean available;
+
+    @OneToMany(mappedBy = "book", cascade = {CascadeType.REMOVE}, fetch = FetchType.LAZY, orphanRemoval = true)
+    private Set<BookObj> bookObjs;
+
+
+    public long getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(long iSBN) {
+        ISBN = iSBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNXB() {
+        return NXB;
+    }
+
+    public void setNXB(String NXB) {
+        this.NXB = NXB;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public Set<BookObj> getBookObjs() {
+        return bookObjs;
+    }
+
+    public void setBookObjs(Set<BookObj> bookObjs) {
+        this.bookObjs = bookObjs;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [ISBN=" + ISBN + ", title=" + title + ", author=" + author + "]";
+    }
+
+
 }
