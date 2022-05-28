77876498ae10469eaee12511ee9de4aadcb00867
chienhoangvan <chienhoangvanp@gmail.com>
2022-05-28 15:35:50 +0700

    update show user info

--- src/main/java/com/psl/gems/model/User.java
+++ src/main/java/com/psl/gems/model/User.java
@@ -2,6 +2,7 @@ package com.psl.gems.model;
 
 import lombok.Data;
 
+import java.util.Date;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -15,68 +16,111 @@ import javax.persistence.OneToMany;
 @Data
 @Entity
 public class User {
-		
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int user_id;
-	private String name;
-	private String role="member";
-	private String username;
-	private String password;
-	private boolean enabled=true;
 
-	private String address;
-	@OneToMany(mappedBy = "user", cascade = {CascadeType.REMOVE}, fetch = FetchType.LAZY, orphanRemoval = true)
-	private Set<Issue> issues;
-	
-	public int getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
-		this.role = role;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public boolean isEnabled() {
-		return enabled;
-	}
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
-	public Set<Issue> getIssues() {
-		return issues;
-	}
-	public void setIssues(Set<Issue> issues) {
-		this.issues = issues;
-	}
-	@Override
-	public String toString() {
-		return "User [user_id=" + user_id + ", name=" + name + ", role=" + role + ", username=" + username
-				+ ", password=" + password + "]";
-	}
-	
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+    private String name;
+
+    private Date dateOfBirth;
+
+    private String number;
+    private String role = "member";
+    private String username;
+    private String password;
+    private boolean enabled = true;
+
+    private String address;
+
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.REMOVE}, fetch = FetchType.LAZY, orphanRemoval = true)
+    private Set<Issue> issues;
+
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<Issue> getIssues() {
+        return issues;
+    }
+
+    public void setIssues(Set<Issue> issues) {
+        this.issues = issues;
+    }
+
+    @Override
+    public String toString() {
+        return "User [user_id=" + user_id + ", name=" + name + ", role=" + role + ", username=" + username
+                + ", password=" + password + "]";
+    }
+
+
 }
